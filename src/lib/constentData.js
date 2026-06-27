dd22dca9d29df793bf1c0fbb43f5add104659398
Lalit Kumar <kumar932687@gmail.com>
2026-06-27 16:46:25 +0530

    updaetd code

---
+++ src/lib/constentData.js
@@ -0,0 +1,40 @@
+import certificateImg from "@/app/api/helperfile/Images/CertificateImg";
+import headerImg from "@/app/api/helperfile/Images/headerImg";
+import logo from "@/app/api/helperfile/Images/logo";
+import semkariLogo from "@/app/api/helperfile/Images/semkariLogo";
+
+export const TrsutData={
+    name:"नवचेतना चेरिटेबल ट्रस्ट डूचकवाडा (वाव -थराद)",
+    cityState:"गुजरात-राजस्थान",
+    address:"ऑफिस: डुचकवाड़ा, ताः दियोदर, जी: वाव-थराद",
+    contact:"9737028137 / 9974328474 / 9974900247",
+    contactPerson:"किरण एच. मोदी",
+    trustPresident:"दिनेश एम मोदी,ईश्वरभाई आर पटेल",
+    frameImg:certificateImg,
+    email:"",
+    website:"",
+    regNo:"F/7218/बनासकांठा",
+    logo:logo,
+    RightLogo:null,
+    headerImg:headerImg,
+    topTitle:[
+        "!! जय श्यामबाबाय नमः !!",
+        "!! जय गणेशाय नमः !!",
+        "!! जय कुलदेवी माताएं नमः !!"
+    ]
+}
+
+export const pdfColors={
+bgColor:'#ffffff',
+borderColor:'#eb3694',
+headingColor:'#eb1e23',
+schemeColor:'#1a0f5e',
+infoLabelColor:'#EA1F25',
+infoValueColor:'#1a0f5e',
+filterLabelColor:'#8B0000',
+filterValueColor:'#1a0f5e',
+summaryColor:'#1a0f5e',
+totalCountColor:'#d4af37',
+programHeaderColor:'#1a0f5e',
+programHeaderBorderColor:'#d4af37',
+}
